4ee4a16ed3a8c6a67199f8b6197786a12840fb3d
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-10-17 13:25:30 -0400

    [v000a0] - Debug mode env and Address Sanitizer with clang as compiler added

--- Main_Thread_Start.c
+++ Main_Thread_Start.c
@@ -1,5 +1,17 @@
+#include <malloc.h>
+#include <stdio.h>
 
 int main()
 {
-    return 0;
+    int* x;
+
+    x = malloc(100);
+
+    *(x + 5) = 5;
+
+    printf("%d - %p\n", *x, x);
+
+    free(x);
+
+    return x[5];
 }
---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Droidcat Project
+
+## Installation
+
